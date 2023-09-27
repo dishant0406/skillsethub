dbf58bad6f9f4f1420cc8bc59ffcd7db7c6b7253
sagnik-05 <98590203+sagnik-05@users.noreply.github.com>
2023-09-27 12:26:58 +0530

    build: login page

--- src/components/SignInComponent/index.tsx
+++ src/components/SignInComponent/index.tsx
@@ -1,13 +1,76 @@
-'use client'
+'use client';
+import React, { useState } from 'react';
+import { CountryDropdown } from 'react-country-region-selector';
 
-import React from 'react'
+type Props = {};
 
-type Props = {}
 
-const SignInComponent = (props: Props) => {
+const SignInComponent: React.FC = () =>
+{
+  const [email, setEmail] = useState('');
+  const [country, setCountry] = useState('');
+  const [region, setRegion] = useState('');
+  const [userType, setUserType] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) =>
+  {
+    event.preventDefault();
+    console.log(`Email: ${email}, Country: ${country}, UserType: ${userType}`);
+  };
+  const selectCountry = (val: any) =>
+  {
+    setRegion(val);
+  }
+
   return (
-    <div>SignInComponent</div>
-  )
-}
+    <div className="flex items-center justify-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-white to-90% h-[100vh] w-[100vw]">
+      <div className="bg-white/60 p-8 shadow-md rounded-md w-90 h-[50vh]">
+        <h2 className="text-2xl font-semibold mb-4">Login / Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">Email</label>
+            <input
+              type="email"
+              className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">Country</label>
+           
+            <CountryDropdown
+              value={country}
+              onChange={selectCountry}
+              classes='w-[100%] mt-1 p-2 w-full border border-gray-300 rounded-md'
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">User Type</label>
+            <select
+              className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+              value={userType}
+              onChange={(e) => setUserType(e.target.value)}
+              required
+            >
+              <option value="" disabled>Select User Type</option>
+              <option value="curriculamDeveloper">Curriculam Developer</option>
+              <option value="student">Student</option>
+            </select>
+          </div>
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 transition duration-300"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default SignInComponent
+export default SignInComponent;
